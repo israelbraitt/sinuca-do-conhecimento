b93a2e0964ea9fc3170a773312fecc0fe6ffad85
israelbraitt <59903080+israelbraitt@users.noreply.github.com>
2022-06-26 23:07:58 -0300

    Faz as bolas sumirem ao entrar nos buracos

--- scripts/taco.gd
+++ scripts/taco.gd
@@ -1,17 +1,14 @@
 extends KinematicBody2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 func _process(delta):
-	var no = get_node("/root/cena_principal/bola_branca/Sprite")
+	var no = get_node("/root/tela_sinuca/bola_branca/Sprite")
+	var position = no.global_position
 	look_at(get_global_mouse_position())
-	print(position)
---
+++ scripts/tela_sinuca.gd
@@ -0,0 +1,397 @@
+extends Node2D
+
+signal bola1_no_buraco
+signal bola2_no_buraco
+signal bola3_no_buraco
+signal bola4_no_buraco
+signal bola5_no_buraco
+signal bola6_no_buraco
+signal bola7_no_buraco
+signal bola8_no_buraco
+signal bola9_no_buraco
+signal bola10_no_buraco
+signal bola11_no_buraco
+signal bola12_no_buraco
+signal bola13_no_buraco
+signal bola14_no_buraco
+signal bola15_no_buraco
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_buraco1_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
+
+
+func _on_buraco2_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
+
+
+func _on_buraco3_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
+
+
+func _on_buraco4_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
+
+
+func _on_buraco5_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
+
+
+func _on_buraco6_body_entered(body):
+	if(body.name != "bola1"):
+		body.queue_free()
+		emit_signal("bola1_no_buraco")
+	
+	elif(body.name != "bola2"):
+		body.queue_free()
+		emit_signal("bola2_no_buraco")
+	
+	elif(body.name != "bola3"):
+		body.queue_free()
+		emit_signal("bola3_no_buraco")
+	
+	elif(body.name != "bola4"):
+		body.queue_free()
+		emit_signal("bola4_no_buraco")
+	
+	elif(body.name != "bola5"):
+		body.queue_free()
+		emit_signal("bola5_no_buraco")
+	
+	elif(body.name != "bola6"):
+		body.queue_free()
+		emit_signal("bola6_no_buraco")
+	
+	elif(body.name != "bola7"):
+		body.queue_free()
+		emit_signal("bola7_no_buraco")
+	
+	elif(body.name != "bola8"):
+		body.queue_free()
+		emit_signal("bola8_no_buraco")
+	
+	elif(body.name != "bola9"):
+		body.queue_free()
+		emit_signal("bola9_no_buraco")
+	
+	elif(body.name != "bola10"):
+		body.queue_free()
+		emit_signal("bola10_no_buraco")
+	
+	elif(body.name != "bola11"):
+		body.queue_free()
+		emit_signal("bola11_no_buraco")
+	
+	elif(body.name != "bola12"):
+		body.queue_free()
+		emit_signal("bola12_no_buraco")
+	
+	elif(body.name != "bola13"):
+		body.queue_free()
+		emit_signal("bola13_no_buraco")
+	
+	elif(body.name != "bola14"):
+		body.queue_free()
+		emit_signal("bola14_no_buraco")
+	
+	elif(body.name != "bola15"):
+		body.queue_free()
+		emit_signal("bola15_no_buraco")
